0b01d3793b8c91454e5e54e7e110f7928dd2ecf7
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-15 02:43:33 +0800

    [Tracy] finish AnswerTest validate

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,7 +1,14 @@
 package tw.core;
 
 
+import com.sun.org.apache.xerces.internal.util.PropertyState;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tw.core.exception.AnswerFormatIncorrectException;
+import tw.core.model.Record;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Created by jxzhong on 2017/9/23.
@@ -14,4 +21,32 @@ public class AnswerTest {
         actualAnswer = Answer.createAnswer("1 2 3 4");
     }
 
+    @Test
+    public void should_get_correct_answer_format_when_call_validate_given_1234(){
+        try {
+            actualAnswer.validate();
+        } catch (AnswerFormatIncorrectException e) {
+            fail("should given validate");
+        }
+    }
+
+    @Test
+    public void should_get_incorrect_answer_format_when_call_validate_given_012(){
+        Answer answer = Answer.createAnswer("0 1 2");
+        try {
+            answer.validate();
+        } catch (AnswerFormatIncorrectException e) {
+        }
+    }
+
+
+    @Test
+    public void should_get_incorrect_answer_format_when_call_validate_given_12345(){
+        Answer answer = Answer.createAnswer("1 2 3 4 5");
+        try {
+            answer.validate();
+        } catch (AnswerFormatIncorrectException e) {
+            fail("should given validate");
+        }
+    }
 }
