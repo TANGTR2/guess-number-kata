54fc5e79792878970195e9158c76720eae265291
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-15 02:47:43 +0800

    [Tracy] finish AnswerTest check

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -49,4 +49,43 @@ public class AnswerTest {
             fail("should given validate");
         }
     }
+    @Test
+    public void should_get_4A0B_when_call_check_given_1234(){
+        //given
+        Answer answer = Answer.createAnswer("1 2 3 4");
+        // when
+        Record record = actualAnswer.check(answer);
+        //then
+        assertThat(record.getValue(), is("4A0B"));
+    }
+
+    @Test
+    public void should_get_2A2B_when_call_check_given_1243(){
+        //given
+        Answer answer = Answer.createAnswer("1 2 4 3");
+        //when
+        Record record = actualAnswer.check(answer);
+        //then
+        assertThat(record.getValue(), is("2A2B"));
+    }
+
+    @Test
+    public void should_get_0A0B_when_call_check_given_0000(){
+        //given
+        Answer answer = Answer.createAnswer("5 6 7 8");
+        //when
+        Record record = actualAnswer.check(answer);
+        //then
+        assertThat(record.getValue(), is("0A0B"));
+    }
+
+    @Test
+    public void should_get_0A4B_when_call_check_given_4321(){
+        //given
+        Answer answer = Answer.createAnswer("4 3 2 1");
+        //when
+        Record record = actualAnswer.check(answer);
+        //then
+        assertThat(record.getValue(), is("0A4B"));
+    }
 }
